87bbdfd39eb612608176d2f9968d22a7b9d78ba8
danghang <dangthihang0504@gmail.com>
2020-05-15 22:25:55 +0700

    login, register,create database

---
+++ ProjectManagement/database/migrations/2020_05_15_140908_create_sprints_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSprintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sprints', function (Blueprint $table) {
+            $table->id();
+            $table->string("purposes");
+            $table->string("slug")->unique();
+            $table->dateTime("stated_at")->nullable();
+            $table->dateTime("end_at")->nullable();
+            $table->integer("project_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sprints');
+    }
+}
---
+++ ProjectManagement/database/migrations/2020_05_15_141101_create_todos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string("content");
+            $table->string("slug")->unique();
+            $table->tinyInteger("status")->default(0);
+            $table->integer("task_id");
+            $table->integer("do_by");
+            $table->tinyInteger("priority")->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
