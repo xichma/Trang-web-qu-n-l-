581a74b1786dce01271f89f05f8bda9923484766
Hằng Đặng <dangthihang0504>
2020-05-15 22:25:55 +0700

    login, register,create database

---
+++ ProjectManagement/app/Http/Controllers/Admin/SprintCrudController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\SprintRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class SprintCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class SprintCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    public function setup()
+    {
+        $this->crud->setModel('App\Models\Sprint');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/sprint');
+        $this->crud->setEntityNameStrings('sprint', 'sprints');
+    }
+
+    protected function setupListOperation()
+    {
+        // TODO: remove setFromDb() and manually define Columns, maybe Filters
+        $this->crud->setFromDb();
+    }
+
+    protected function setupCreateOperation()
+    {
+        $this->crud->setValidation(SprintRequest::class);
+
+        // TODO: remove setFromDb() and manually define Fields
+        $this->crud->setFromDb();
+    }
+
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
