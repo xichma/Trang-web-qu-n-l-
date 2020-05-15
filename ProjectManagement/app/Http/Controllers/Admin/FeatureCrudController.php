93ad678a14593c63ef00b03d8d62f01f6d0f106c
danghang <dangthihang0504@gmail.com>
2020-05-15 22:25:55 +0700

    login, register,create database

---
+++ ProjectManagement/app/Http/Controllers/Admin/FeatureCrudController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\FeatureRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class FeatureCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class FeatureCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    public function setup()
+    {
+        $this->crud->setModel('App\Models\Feature');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/feature');
+        $this->crud->setEntityNameStrings('feature', 'features');
+    }
+
+    protected function setupListOperation()
+    {
+        // TODO: remove setFromDb() and manually define Columns, maybe Filters
+        $this->crud->setFromDb();
+    }
+
+    protected function setupCreateOperation()
+    {
+        $this->crud->setValidation(FeatureRequest::class);
+
+        // TODO: remove setFromDb() and manually define Fields
+        $this->crud->setFromDb();
+    }
+
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
